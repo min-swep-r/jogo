2f88b312e052ed24b2d660467553b3873f4570a4
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2024-01-27 13:09:39 -0300

    segndodd envio

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Gott Mit Uns!");
-        System.out.println("teste do commit.");
+        System.out.println("teste do commit:");
     }
 }
