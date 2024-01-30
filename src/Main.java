8deb9553016b15c3ab2540ea262902813c22db3f
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2024-01-30 20:34:38 -0300

    testando git do intellij

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@
 //import java.util
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Gott Mit Uns!");
-        System.out.println("mt estranho o git da IDE, vou testar!");
+        System.out.println("Gott Mit Uns! All already! Ty God!!!");
     }
 }
