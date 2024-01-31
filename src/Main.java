880b47dc622543e2e5b36d91a151e5677c140e78
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2024-01-31 15:19:42 -0300

    Na Branch de Desenvolvimento.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@
 //Primeira chamada
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Gott Mit Uns");
+        System.out.println("Gott Mit Uns!");
     }
 
 }
