c7c1e7c80c86d36eea2aaa2a5c7b056161493d23
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2024-01-30 23:22:42 -0300

    envio

--- rodar.bat
+++ rodar.bat
@@ -1,3 +1,12 @@
 @echo off
 java Jogo/src/main/java/com/mycompany/jogo/Jogo.java
 pause
+
+
+
+tenho que colocar ele compilando com o module e depois executando. como em:
+
+javac --module-path javafx-sdk-18.0.2/lib --add-modules javafx.controls Main.java
+java --module-path javafx-sdk-18.0.2/lib --add-modules javafx.controls Main
+
+com o Java FX que está na pasta!
