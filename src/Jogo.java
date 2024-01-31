e387c6e52ce3dd03ce110c81183a0e9f36c9417b
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2024-01-31 16:03:10 -0300

    construindo, segundo o diagrama

---
+++ src/Jogo.java
@@ -0,0 +1,32 @@
+public class Jogo {
+    // Atributos
+    private Tabuleiro table;
+    private User usuario;
+    private int tamX;
+    private int tamY;
+
+    // Construtor
+    public Jogo(Tabuleiro table, User usuario, int tamX, int tamY) {
+        this.table = table;
+        this.usuario = usuario;
+        this.tamX = tamX;
+        this.tamY = tamY;
+    }
+
+    // Métodos
+    public void iniciaJogo() {
+        // Implementação do método
+    }
+
+    public void verificaJogo() {
+        // Implementação do método
+    }
+
+    public void revelaCel() {
+        // Implementação do método
+    }
+
+    public void marcaCel() {
+        // Implementação do método
+    }
+}
