bf32cff26cd4eee3252b7fe7b20f6801f8eb4270
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2024-02-14 02:31:50 -0300

    rapaz, esa do graphs é pegado

--- src/Graphics.java
+++ src/Graphics.java
@@ -33,8 +33,41 @@ public class Graphics extends Application {
         root.getChildren().add(label);
         StackPane.setAlignment(label, Pos.CENTER);
 
+        // Criando botão "Iniciar"
+        Button button = new Button("Iniciar");
+
+        // Criando layout para o primeiro frame
+        VBox vbox1 = new VBox(10);
+        vbox1.setAlignment(Pos.CENTER);
+        vbox1.getChildren().addAll(label, button);
+        root.getChildren().add(vbox1);
+
+        // Eventos do botão ----------------------------------
+        button.setOnAction(e -> {
+            // Chama o método para exibir o segundo frame, passando a cena do primeiro frame
+            segundoFrame(primaryStage, primaryStage.getScene());
+        });
+
+
         primaryStage.show();
     }
+
+    private void segundoFrame(Stage primaryStage, Scene firstScene) {
+        // Criando botão "Voltar"
+        Button backButton = new Button("Voltar");
+        backButton.setOnAction(e -> {
+            // Volta para o primeiro frame
+            primaryStage.setScene(firstScene);
+        });
+
+        // Criando layout para o segundo frame
+        VBox waitFrame = new VBox(10);
+        waitFrame.setAlignment(Pos.CENTER);
+        waitFrame.getChildren().addAll(new Label("Espere, kkk"), backButton);
+
+        primaryStage.setScene(new Scene(waitFrame, 300, 200));
+    }
+
 
     public static void openWindow() {
         new Thread(() -> launch(Graphics.class)).start(); // Iniciado como uma função à parte!!!
@@ -44,3 +77,4 @@ public class Graphics extends Application {
         launch(args);
     }
 }
+
